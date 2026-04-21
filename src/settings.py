72d45d8a57e5bae2fa4a531c28583cb989cdb49f
Matěj Zeman <zemanm40@fit.cvut.cz>
2026-04-21 14:37:36 +0200

    hw05: fix wrong material names in bunny scene

--- src/settings.py
+++ src/settings.py
@@ -5,17 +5,17 @@ DEVICE = "gpu"  # "cpu" or "gpu"
 # for CPU python run:
 CPU_DIMENSION = 500
 # for GPU runs:
-GPU_DIMENSION = 150
+GPU_DIMENSION = 1024
 # GPU_DIMENSION = 5760
 
 # SCENE_NAME = "box-advanced"
 
-SCENE_NAME = "box-spheres"
+SCENE_NAME = "bunny"
 SAMPLES = 16
 DENOISE = False
 MAX_BOUNCES = 16
 # more than 32 is overkill, but counting on attenuation
 IMG_FORMAT = "jpg"
-USE_BVH_CACHE = True
-PRINT_STATS = False
-RENDER_NON_BVH_STATS = False
+USE_BVH_CACHE = False
+PRINT_STATS = True
+RENDER_NON_BVH_STATS = True
